143a2cc7afe606cff93d18ede3f96814a13523b9
lyxint <lyxint@gmail.com>
2011-09-15 00:12:32 +0800

    update

--- functions.php
+++ functions.php
@@ -5,23 +5,22 @@ function l_page_menu_args( $args ) {
 }
 add_filter( 'wp_page_menu_args', 'l_page_menu_args' );
 
-if ( function_exists('register_sidebar') )
-register_sidebar(array(
-    'name' => 'Sidebar',
-    'before_widget' => '',
-    'after_widget' => '',
-    'before_title' => '',
-    'after_title' => '',
-));
-
-
-if ( function_exists('register_sidebar') )
-register_sidebar(array(
-    'name' => 'Index Description',
-    'before_widget' => '',
-    'after_widget' => '',
-    'before_title' => '',
-    'after_title' => '',
-));
+if ( function_exists('register_sidebar') ) {
+    register_sidebar(array(
+        'name' => 'Sidebar',
+        'before_widget' => '',
+        'after_widget' => '',
+        'before_title' => '',
+        'after_title' => '',
+    ));
+    
+    register_sidebar(array(
+        'name' => 'Index Description',
+        'before_widget' => '',
+        'after_widget' => '',
+        'before_title' => '',
+        'after_title' => '',
+    ));
+}
 
 ?>
