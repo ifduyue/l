06da91292818f059c89d4409f15c5588b5100d45
lyxint <lyxint@gmail.com>
2011-09-15 00:03:13 +0800

    google analytics

--- footer.php
+++ footer.php
@@ -4,5 +4,18 @@
     </div>
 </div>
 <!-- Can put web stats code here -->
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-9750080-2']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </body>
 </html>
