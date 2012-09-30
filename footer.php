0f4422b18fa259aaaa0567c492e9144ea7c8ef74
Elyes Du <lyxint@gmail.com>
2012-09-30 18:02:10 +0800

    powered by wordpress

--- footer.php
+++ footer.php
@@ -1,6 +1,7 @@
 
     <div id="footer">
         <p>Copyright &copy; <?php echo date("Y") ?> <a href="<?php echo get_option('home'); ?>"><?php bloginfo('name'); ?></a>. All Rights Reserved.</p>
+        <p>Powered by <a target="_blank" href="http://wordpress.org" title="Semantic Personal Publishing Platform" rel="generator">WordPress</a>.</p>
     </div>
 </div>
 <p><?php wp_footer() ?></p>
